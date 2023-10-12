06d6e6e7350a65ddc6cacd410f9f944af2b57ec4
EliasTorres <dev_e_t@outlook.com>
2023-10-12 20:29:25 -0300

    add get and set Cliente

--- FinalLaboratorioII/entidades/Cliente.cs
+++ FinalLaboratorioII/entidades/Cliente.cs
@@ -5,28 +5,53 @@ namespace FinalLaboratorioII.entidades
     internal class Cliente
     {
         private int id_cliente { get; set; }
-        private string cliente { get; set; }
+        private string nombreCliente { get; set; }
         private string cuit { get; set; }
         private string domicilio { get; set; }
-        private Localidad id_localidad { get; set; }
+        private int id_localidad { get; set; }
         private string telefono { get; set; }
         private string correo { get; set; }
 
         public Cliente()
         { }
 
-        public Cliente(int id_cliente, string cliente, string cuit, string domicilio, Localidad id_localidad, string telefono, string correo)
+        public Cliente(int id_cliente, string cliente, string cuit, string domicilio, int id_localidad, string telefono, string correo)
         {
             this.id_cliente = id_cliente;
-            this.cliente = cliente ?? throw new ArgumentNullException(nameof(cliente));
+            this.nombreCliente = cliente ?? throw new ArgumentNullException(nameof(cliente));
             this.cuit = cuit ?? throw new ArgumentNullException(nameof(cuit));
             this.domicilio = domicilio ?? throw new ArgumentNullException(nameof(domicilio));
-            this.id_localidad = id_localidad ?? throw new ArgumentNullException(nameof(id_localidad));
+            this.id_localidad = id_localidad;
             this.telefono = telefono ?? throw new ArgumentNullException(nameof(telefono));
             this.correo = correo ?? throw new ArgumentNullException(nameof(correo));
         }
 
-        
+        public string NombreCliente
+        {
+            get{return this.nombreCliente;} set { this.nombreCliente = value; }
+        }
+        public string Cuit
+        {
+            get { return this.cuit; } set { this.cuit = value; }
+        }
+        public string Domicilio
+        {
+            get { return this.domicilio; } set { this.domicilio = value; }
+        }
+        public int Id_localidad
+        {
+            get{return this.id_localidad;} set{ this.id_localidad = value;}            
+        }
+
+        public string Telefono
+        {
+            get { return this.telefono; } set { this.telefono = value;}
+        }
+
+        public string Correo
+        {
+            get { return this.correo; } set { this.correo = value;}
+        }
 
     }
 }
---
+++ FinalLaboratorioII/servicios/ServicioCliente.cs
@@ -0,0 +1,54 @@
+﻿using FinalLaboratorioII.entidades;
+using System;
+namespace FinalLaboratorioII.servicios
+{
+    internal class ServicioCliente
+    {
+        public ServicioCliente()
+        {
+        }
+
+        public Cliente crearCliente()
+        {
+            Cliente cliente = new Cliente();
+            Console.WriteLine("Ingrese cliente: ");
+            cliente.NombreCliente = Console.ReadLine();
+            Console.WriteLine("Ingrese C.U.I.T.: ");
+            cliente.Cuit = Console.ReadLine();
+            Console.WriteLine("Ingrese domicilio del cliente: ");
+            cliente.Domicilio = Console.ReadLine();
+            Console.WriteLine("Ingrese localidad del cliente: ");
+            cliente.NombreCliente = Console.ReadLine();
+            Console.WriteLine("Ingrese telefono:");
+            cliente.NombreCliente = Console.ReadLine();
+            Console.WriteLine("Ingrese correo electrónico:");
+            cliente.NombreCliente = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return new Cliente();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
